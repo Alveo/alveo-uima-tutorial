19f3e102da64238fad53611cf1892189f9d389d6
Andrew MacKinlay <am@akmy.net>
2014-04-14 17:47:56 +1000

    initial checkin of two approaches for supplying POS tags to Alveo

---
+++ src/main/java/au/edu/alveo/uima/tutorial/DKProPosConverter.java
@@ -0,0 +1,71 @@
+package au.edu.alveo.uima.tutorial;
+
+import com.nicta.uimavlab.conversions.UIMAToAlveoAnnConverter;
+import com.nicta.vlabclient.TextRestAnnotation;
+import org.apache.uima.cas.Feature;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.TypeSystem;
+import org.apache.uima.cas.text.AnnotationFS;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/** Class to handle conversion of POS annotations more sensibly. By default, there are broad
+ * leaf annotations for different POS types (V, N etc). If we want all POS annotations to have
+ * the same type in Alveo, we need to create a custom map
+ *
+ * (alternatively we could also write our own UIMA CasAnnotator to output UIMA types which
+ * are more straightforwardly convertible. We merely need to have the desired type
+ * encoded in a feature (or be happy with the URL automatically generated from its
+ * actual type name in {@link com.nicta.uimavlab.conversions.DefaultUIMAToAlveoAnnConverter},
+ * and the desired label encoded in another feature)
+ */
+public class DKProPosConverter implements UIMAToAlveoAnnConverter {
+	private static final Logger LOG = LoggerFactory.getLogger(DKProPosConverter.class);
+
+	private TypeSystem ts = null;
+	private Feature labelFeature;
+	private Type primaryType;
+	private Set<Type> allowedTypes = new TreeSet<Type>();
+
+	private static final String LABEL_FEATURE_NAME = "de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS:PosValue";
+	private static final String PRIMARY_TYPE_NAME = "de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS";
+
+	@Override
+	public void setTypeSystem(TypeSystem ts) {
+		if (ts.equals(this.ts))
+			return;
+		this.ts = ts;
+		labelFeature = this.ts.getFeatureByFullName(LABEL_FEATURE_NAME);
+		if (labelFeature == null)
+			LOG.error("Type system had no feature {}", LABEL_FEATURE_NAME);
+		primaryType = this.ts.getType(PRIMARY_TYPE_NAME);
+		if (primaryType == null)
+			LOG.error("Type system had no type named {}", PRIMARY_TYPE_NAME);
+		allowedTypes.clear();
+		if (primaryType != null)
+			allowedTypes.addAll(ts.getProperlySubsumedTypes(primaryType));
+	}
+
+	@Override
+	public TextRestAnnotation convertToAlveo(AnnotationFS ann) throws InvalidAnnotationTypeException, NotInitializedException {
+		if (!handlesTypeName(ann.getType().getName()))
+			throw new InvalidAnnotationTypeException("Annotation " + ann + " is not a subtype of " + PRIMARY_TYPE_NAME);
+		return new TextRestAnnotation(PRIMARY_TYPE_NAME, ann.getFeatureValueAsString(labelFeature), ann.getBegin(), ann.getEnd());
+	}
+
+	@Override
+	public String getAlveoTypeUriForTypeName(String uimaTypeName) {
+		return PRIMARY_TYPE_NAME;
+	}
+
+	@Override
+	public boolean handlesTypeName(String uimaTypeName) {
+		Type uimaType = ts.getType(uimaTypeName);
+		if (uimaType == null)
+			return false;
+		return allowedTypes.contains(uimaType);
+	}
+}
---
+++ src/main/java/au/edu/alveo/uima/tutorial/PosTagDemoAdvanced.java
@@ -0,0 +1,90 @@
+package au.edu.alveo.uima.tutorial;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.nicta.uimavlab.ItemAnnotationUploader;
+import com.nicta.uimavlab.ItemListCollectionReader;
+import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger;
+import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpSegmenter;
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * Created by amack on 14/04/14.
+ */
+public class PosTagDemoAdvanced {
+	private static final Logger LOG = LoggerFactory.getLogger(PosTagDemoAdvanced.class);
+
+	protected static class CLParams {
+
+		@Parameter(names = {"-u", "--server-url"}, required = true, description = "Base URL of HCS vLab server")
+		private String serverUrl;
+
+		@Parameter(names = {"-k", "--api-key"}, required = true,
+				description = "API key for for your user account, obtainable from the web interface")
+		private String apiKey;
+
+		@Parameter(names = { "--help", "-h", "-?" }, help = true, description = "Display this help text")
+		private boolean help;
+
+		@Parameter(names = { "--descriptor-dir"}, required = false,
+				description = "If provided, a directory where descriptors will be written")
+		private String descriptorDir = null;
+
+		@Parameter(names = { "-o", "--xmi-output-dir"}, required = true,
+				description = "The directory where the XMI files produced will be written")
+		private String xmiDir;
+
+		@Parameter(names = { "-i", "--item-list-id"}, required = true, description =
+				"The item list ID to convert to XMI")
+		private String itemListId;
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		CLParams params = new CLParams();
+		JCommander jcom = new JCommander(params, args);
+		jcom.setProgramName(PosTagDemo.class.getName());
+		if (params.help) {
+			jcom.usage();
+			return;
+		}
+		runPipeline(params.serverUrl, params.apiKey, params.xmiDir, params.itemListId);
+	}
+
+	private static void runPipeline(String serverUrl, String apiKey, String xmiDir, String itemListId)
+			throws UIMAException, IOException {
+		String[] extraConverters = new String[] {
+				"au.edu.alveo.uima.tutorial.DKProPosConverter"
+		};
+		CollectionReaderDescription reader = ItemListCollectionReader.createDescription(
+				ItemListCollectionReader.PARAM_VLAB_BASE_URL, serverUrl,
+				ItemListCollectionReader.PARAM_VLAB_API_KEY, apiKey,
+				ItemListCollectionReader.PARAM_VLAB_ITEM_LIST_ID, itemListId,
+				ItemListCollectionReader.PARAM_INCLUDE_RAW_DOCS, false,
+				ItemListCollectionReader.PARAM_ANNOTATION_CONVERTERS, extraConverters);
+		AnalysisEngineDescription segmenter = AnalysisEngineFactory.createEngineDescription(OpenNlpSegmenter.class);
+		AnalysisEngineDescription posTagger = AnalysisEngineFactory.createEngineDescription(OpenNlpPosTagger.class);
+
+
+		String[] uploadableTypes = new String[] {
+				"de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS"
+		};
+		AnalysisEngineDescription uploader = AnalysisEngineFactory.createEngineDescription(ItemAnnotationUploader.class,
+				ItemAnnotationUploader.PARAM_VLAB_BASE_URL, serverUrl,
+				ItemAnnotationUploader.PARAM_VLAB_API_KEY, apiKey,
+				ItemAnnotationUploader.PARAM_ANNOTATION_CONVERTERS, extraConverters,
+				ItemAnnotationUploader.PARAM_UPLOADABLE_UIMA_TYPE_NAMES, uploadableTypes);
+		AnalysisEngineDescription aggAe = AnalysisEngineFactory.createEngineDescription(segmenter, posTagger, uploader);
+		SimplePipeline.runPipeline(reader, aggAe);
+	}
+
+
+}
